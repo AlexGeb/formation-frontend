9a28ae25cd469f2e56f99247afc84af707a36ed2
AlexGeb <alex.behaghel@gmail.com>
2018-01-05 09:59:55 +0100

    ajout de la JSDoc

--- 02-nodejs/console.js
+++ 02-nodejs/console.js
@@ -14,14 +14,20 @@ const app = (function(dataService) {
     { libelle: "Détail d'une session", action: askForSessionId }
   ];
 
+  /** affiche le menu
+   * @param {Object[]} menu tableau d'objets de la forme : {libelle:String,action:Function}
+   */
   function displayMenu(menu) {
     console.log('*** Application Conférence ***');
-    for (var key in menu) {
+    for (let key in menu) {
       const id = parseInt(key) + 1;
       console.log(id + '. ' + menu[key].libelle);
     }
   }
 
+  /** fonction principale, à invoquer pour démarrer l'appli
+   * elle affiche un menu et pose une question à l'utilisateur
+   */
   function start() {
     displayMenu(menu);
     rl.question('Entrez une option (1,2,3 ou 4) : \n', answer => {
@@ -32,11 +38,16 @@ const app = (function(dataService) {
       }
     });
   }
+
+  /** affiche un message et ferme le readline */
   function badInput() {
     console.log('bad input, good bye');
     rl.close();
   }
 
+  /** affiche le prénom et le nom de tous les speakers
+   * @param {Function} callback
+   */
   function listerPresentateurs(callback) {
     dataService.listerTousLesPresentateurs().forEach(sp => {
       console.log(sp.firstname + ' ' + sp.lastname);
@@ -44,6 +55,9 @@ const app = (function(dataService) {
     callback();
   }
 
+  /** affiche le prénom et le nom de tous les top speakers
+   * @param {Function} callback
+   */
   function listerTopPresentateurs(callback) {
     dataService.listerTopPresentateurs().forEach(sp => {
       console.log(sp.firstname + ' ' + sp.lastname);
@@ -51,6 +65,9 @@ const app = (function(dataService) {
     callback();
   }
 
+  /** affiche le titre de toute les sessions
+   * @param {Function} callback
+   */
   function listerSessions(callback) {
     dataService.listerToutesLesSessions().forEach(sess => {
       console.log(sess.title);
@@ -58,6 +75,9 @@ const app = (function(dataService) {
     callback();
   }
 
+  /** demande à l'utilisateur un id de sesssion et renvoie sa description et le noms de ses présentateurs
+   * @param {Function} callback
+   */
   function askForSessionId(callback) {
     rl.question('Entrez un identifiant de session : \n', id => {
       const session = dataService.trouverUneSession(id);
@@ -72,10 +92,7 @@ const app = (function(dataService) {
       callback();
     });
   }
-
-  return {
-    start
-  };
+  return { start };
 })(dataService);
 
 // Demarrage de l'appli console :
--- 02-nodejs/service.js
+++ 02-nodejs/service.js
@@ -1,13 +1,30 @@
 const data = require('./data/devfest-2015');
+
+/** listerTousLesPresentateurs
+ * @returns liste tous les présentateurs
+ */
 function listerTousLesPresentateurs() {
   return data.speakers;
 }
+
+/** listerToutesLesSessions
+ * @returns liste toutes les sessions
+ */
 function listerToutesLesSessions() {
   return data.sessions;
 }
+
+/** trouverUneSession
+ * @param {string} idSession
+ * @returns la session avec l'id idSession
+ */
 function trouverUneSession(idSession) {
   return data.sessions.find(session => session.id == idSession);
 }
+
+/** listerTousLesPresentateurs
+ * @returns liste toutes les sessions
+ */
 function listerTopPresentateurs() {
   return data.speakers.filter(speaker => speaker.topspeaker);
 }
